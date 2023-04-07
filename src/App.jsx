757731b2e790e15f7b07444718675e3fb565d6fa
Rathin7 <rathinbaraiRB637@gmail.com>
2023-04-08 01:45:50 +0600

    First initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-indigo-600 text-4xl'>Welcome to RKB-Pharma</h1>
+    </div>
+  );
+};
+
+export default App;
